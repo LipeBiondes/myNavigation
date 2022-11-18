067fbbeb61e579ed1467a5310bd99a56788e6fe3
Lipe biondes <92653779+LipeBiondes@users.noreply.github.com>
2022-11-17 22:51:53 -0300

    Update

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+
+import Contacts from './src/pages/Contacts'
+import Information from './src/pages/Information'
+
+const Drawer = createDrawerNavigator()
 
 export default function App() {
   return (
-    
-  );
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Contacts" component={Contacts} />
+        <Drawer.Screen name="Information" component={Information} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
 }
---
+++ src/pages/Information/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Information() {
+  return (
+    <View>
+      <Text>Muito fedida</Text>
+    </View>
+  )
+}
